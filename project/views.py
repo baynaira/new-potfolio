d82cb996e0ef1b29c68c55d2822a14d2b5789afc
bay naira <me@gmail.com>
2024-06-08 14:08:07 -0700

    first comit

---
+++ project/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
