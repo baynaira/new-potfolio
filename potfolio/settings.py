d986183a89b7f5fc9c50d42760d034ccfd183231
bay naira <me@gmail.com>
2024-06-20 14:40:29 -0700

    add a new project setting

--- potfolio/settings.py
+++ potfolio/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'home/baycoineleniyan/new-potfolio/project/static/'
+STATIC_ROOT = 'potfolio/baycoineleniyan/potfolio/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
